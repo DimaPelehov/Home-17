e9aa968121cfecfe7415a71b1b5af87aaae845b8
Dmytro <pelexov.mitya@gmail.com>
2023-11-23 14:27:05 +0200

    fix problems, maybe

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // -------------------ДЗ 17.1---------------------
 function trimString(str, maxLength) {
-  if (str === null || str === "") {
-    alert("Ви не ввели текст!");
-  }
-
-  let long = str.length;
-
-  let trueLong = str.slice(0, maxLength);
-
-  let falseLong = "...";
-
-  if (long > maxLength) {
-    return `${trueLong}${falseLong}`;
+  if (!str) {
+    alert("Ви не ввели символів,спробуйте ще раз!");
   } else {
-    return trueLong;
+    let long = str.length;
+
+    let trueLong = str.slice(0, maxLength);
+
+    let falseLong = "...";
+
+    if (long > maxLength) {
+      console.log(`${trueLong}${falseLong}`);
+    } else {
+      console.log(trueLong);
+    }
   }
 }
 
-console.log(trimString(prompt("enter text"), 8));
+trimString(prompt("Введіть будь-які символи"), 8);
 
 // Варіант коротше-----
 // function truncate(str, maxlength) {
@@ -25,6 +25,7 @@ console.log(trimString(prompt("enter text"), 8));
 // }
 
 // console.log(truncate(prompt("enter text"), 8));
+
 // ------------------ДЗ 17.2-----------------------
 
 // -----Варіант 1 (довший)---------
@@ -51,26 +52,24 @@ console.log(trimString(prompt("enter text"), 8));
 // -----Варіант 2 (коротший)---------
 
 function checkRandNumb(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  let number = Math.floor(Math.random() * (max - min)) + min;
-  console.log(number);
-
-  if (number % 2 === 0) {
-    return "Число парне";
-  } else if (number === 0) {
-    return "Число 0, спробуйте ще раз";
-  } else {
-    return "Число непарне";
+  if (min && max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let number = Math.floor(Math.random() * (max - min)) + min;
+    console.log(number);
+    if (number % 2 === 0) {
+      console.log("Число парне");
+    } else if (number % 2 != 0) {
+      console.log("Число непарне");
+    }
+  } else if (!min || !max) {
+    alert("Некоректне значення, спробуйте ще раз");
   }
 }
 
-console.log(
-  checkRandNumb(
-    +prompt("Введіть мінімальне число діапазону рандому"),
-    +prompt("Введіть максималне число діапазону рандому")
-  )
+checkRandNumb(
+  +prompt("Введіть мінімальне число діапазону рандому"),
+  +prompt("Введіть максималне число діапазону рандому")
 );
 
 // ------Варіант 3 (діапазон йде від 0 до max)----
@@ -87,3 +86,5 @@ console.log(
 // }
 
 // console.log(checkRandom(100));
+
+// -------
